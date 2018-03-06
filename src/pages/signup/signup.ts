53597d25a1e3952cecd49d825e81cfe27474ab36
schurmann <schurms@bigpond.com>
2018-03-06 20:38:26 +1000

    Added Logos to signup and login

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
 import { TabsPage } from '../tabs/tabs';
-import { ProjectService } from '../../services/project.service';
 
 @Component({
   selector: 'page-signup',
@@ -13,23 +12,14 @@ export class SignupPage {
   username: string;
   password: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-              private projectService: ProjectService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   onSignup(form: NgForm) {
-    // Load some dummy data
-    var newStartDate = new Date('2018-01-01').toISOString().substring(0,10);
-    var newEndDate = new Date('2018-01-01').toISOString().substring(0,10);
-
-    this.projectService.createProject("Project 1", "Manager 1", "This project is about ABC.", newStartDate, newEndDate);
-    this.projectService.createProject("Project 2", "Manager 2", "This project is about ABC.", newStartDate, newEndDate);
-
     this.username = form.value.username;
     this.password = form.value.password;
     // validate username and password combination
     // console.log(this.username, this.password);
-
     this.navCtrl.push(TabsPage);
   }
 
