1da1ca2c91b6f3dc12cfa7c9f9d587a630d1054f
schurmann <schurms@bigpond.com>
2018-03-09 21:08:05 +1000

    Update logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import {App, MenuController, NavController, Platform} from 'ionic-angular';
+import { MenuController, NavController, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -15,9 +15,8 @@ export class MyApp {
 
   constructor(platform: Platform,
               statusBar: StatusBar,
-              splashScreen: SplashScreen ,
-              private menuCtrl: MenuController,
-              public app: App) {
+              splashScreen: SplashScreen,
+              private menuCtrl: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -28,8 +27,7 @@ export class MyApp {
 
   onLogout() {
     this.menuCtrl.close();
-    const root = this.app.getRootNav();
-    root.popToRoot();
+    this.navCtrl.popToRoot();
 
   }
 }
