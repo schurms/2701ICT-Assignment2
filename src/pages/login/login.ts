4c6daed647e1e7d6c4ef933708ac5466758b5f6a
schurmann <schurms@bigpond.com>
2018-03-30 12:25:45 +1000

    Add loading controller

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { LoadingController, NavController, NavParams } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
 import { TabsPage } from '../tabs/tabs';
 
@@ -12,20 +12,26 @@ export class LoginPage {
   username: string;
   password: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.password = "";
-    this.username = "";
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private loadingCtrl: LoadingController) {
   }
 
-  ionViewDidEnter() {
-    this.username = ""
-    this.password = ""
-  }
-
+  // Accept input and Log the user on
   onLogin(form: NgForm) {
-    // this.username = form.value.username;
-    //  this.password = form.value.password;
-    this.navCtrl.push(TabsPage);
+    const loading = this.loadingCtrl.create({
+      content: 'Logging you in...'
+    });
+    loading.present();
+
+
+    // Timeout Loading Controller
+    setTimeout(() => {
+      loading.dismiss();
+      this.navCtrl.push(TabsPage);
+    }, 700);
+    this.username = form.value.username;
+    this.password = form.value.password;
   }
 
 }
